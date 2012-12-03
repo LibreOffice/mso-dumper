63c8e0ec7e648f249121b6525bef3ea598828be6
Miklos Vajna <vmiklos@suse.cz>
2012-12-03 13:13:01 +0100

    dump OfficeArtSplitMenuColorContainer

--- src/docdraw.py
+++ src/docdraw.py
@@ -23,7 +23,7 @@ class OfficeArtRecordHeader(DOCDirStream):
         print '<%s type="OfficeArtRecordHeader" offset="%d" size="%d bytes">' % (self.name, self.pos, OfficeArtRecordHeader.size)
         buf = self.readuInt16()
         self.printAndSet("recVer", buf & 0x000f) # 1..4th bits
-        self.printAndSet("recInstance", buf & 0xfff0) # 5..16th bits
+        self.printAndSet("recInstance", (buf & 0xfff0) >> 4) # 5..16th bits
 
         self.printAndSet("recType", self.readuInt16())
         self.printAndSet("recLen", self.readuInt32())
@@ -84,6 +84,46 @@ class OfficeArtFDGGBlock(DOCDirStream):
         print '</%s>' % self.name
         self.officeArtDggContainer.pos = self.pos
 
+class MSOCR(DOCDirStream):
+    """The MSOCR record specifies either the RGB color or the scheme color index."""
+    size = 4
+    def __init__(self, parent):
+        DOCDirStream.__init__(self, parent.bytes)
+        self.pos = parent.pos
+        self.parent = parent
+
+    def dump(self):
+        print '<msocr type="MSOCR" offset="%d" size="%d">' % (self.pos, MSOCR.size)
+        buf = self.readuInt32()
+        self.printAndSet("red",           buf & 0x000000ff) # 1..8th bits
+        self.printAndSet("green",        (buf & 0x0000ff00) >> 8) # 9..16th bits
+        self.printAndSet("blue",         (buf & 0x00ff0000) >> 16) # 17..24th bits
+        self.printAndSet("unused1",      (buf & 0x07000000) >> 24) # 25..27th bits
+        self.printAndSet("fSchemeIndex", (buf & 0x08000000) >> 27) # 28th bits
+        self.printAndSet("unused2",      (buf & 0xf0000000) >> 28) # 29..32th bits
+        print '</msocr>'
+        self.parent.pos = self.pos
+
+class OfficeArtSplitMenuColorContainer(DOCDirStream):
+    """The OfficeArtSplitMenuColorContainer record specifies a container for the colors that were most recently used to format shapes."""
+    def __init__(self, officeArtDggContainer, name):
+        DOCDirStream.__init__(self, officeArtDggContainer.bytes)
+        self.name = name
+        self.pos = officeArtDggContainer.pos
+        self.officeArtDggContainer = officeArtDggContainer
+
+    def dump(self):
+        print '<%s type="OfficeArtSplitMenuColorContainer" offset="%d">' % (self.name, self.pos)
+        OfficeArtRecordHeader(self, "rh").dump()
+        self.head = OfficeArtFDGG(self, "head")
+        self.head.dump()
+        for i in ["fill", "line", "shadow", "3d"]:
+            print '<smca type="%s">' % i
+            MSOCR(self).dump()
+            print '</smca>'
+        print '</%s>' % self.name
+        self.officeArtDggContainer.pos = self.pos
+
 class OfficeArtDggContainer(DOCDirStream):
     """The OfficeArtDggContainer record type specifies the container for all the OfficeArt file records that contain document-wide data."""
     def __init__(self, officeArtContent, name):
@@ -92,10 +132,24 @@ class OfficeArtDggContainer(DOCDirStream):
         self.pos = officeArtContent.pos
         self.officeArtContent = officeArtContent
 
+    def getNextType(self):
+        return self.getuInt16(pos = self.pos + 2)
+
     def dump(self):
         print '<%s type="OfficeArtDggContainer" offset="%d">' % (self.name, self.pos)
         OfficeArtRecordHeader(self, "rh").dump()
-        OfficeArtFDGGBlock(self, "drawingGroup").dump()
+        if self.getNextType() == 0xf006:
+            OfficeArtFDGGBlock(self, "drawingGroup").dump()
+        if self.getNextType() == 0xf001:
+            print '<todo what="OfficeArtDggContainer: unhandled OfficeArtBStoreContainer"/>'
+        if self.getNextType() == 0xf00b:
+            print '<todo what="OfficeArtDggContainer: unhandled OfficeArtFOPT"/>'
+        if self.getNextType() == 0xf122:
+            print '<todo what="OfficeArtDggContainer: unhandled OfficeArtTertiaryFOPT"/>'
+        if self.getNextType() == 0xf11a:
+            print '<todo what="OfficeArtDggContainer: unhandled OfficeArtColorMRUContainer"/>'
+        if self.getNextType() == 0xf11e:
+            OfficeArtSplitMenuColorContainer(self, "splitColors").dump()
         print '</%s>' % self.name
 
 # vim:set filetype=python shiftwidth=4 softtabstop=4 expandtab:
