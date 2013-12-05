726c606bfb1ce0171d867d6c9bf7891c77d6dd67
Thorsten Behrens <tbehrens@suse.com>
2013-12-05 01:33:17 +0100

    Move codebase to MPL.
    SUSE-contributed code is MPLv2, other patch contributors have
    consented to new license.
    
    Signed-off-by: Kohei Yoshida <kohei.yoshida@gmail.com>
    Signed-off-by: Miklos Vajna <vmiklos@suse.cz>
    Signed-off-by: Noel Power <noel.power@suse.com>
    Signed-off-by: Sergey Kishchenko <voidwrk@gmail.com>

--- xls-dump.py
+++ xls-dump.py
@@ -1,30 +1,9 @@
 #!/usr/bin/env python2
-########################################################################
 #
-#  Copyright (c) 2010 Kohei Yoshida
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
-#  Permission is hereby granted, free of charge, to any person
-#  obtaining a copy of this software and associated documentation
-#  files (the "Software"), to deal in the Software without
-#  restriction, including without limitation the rights to use,
-#  copy, modify, merge, publish, distribute, sublicense, and/or sell
-#  copies of the Software, and to permit persons to whom the
-#  Software is furnished to do so, subject to the following
-#  conditions:
-#
-#  The above copyright notice and this permission notice shall be
-#  included in all copies or substantial portions of the Software.
-#
-#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-#  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-#  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-#  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-#  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-#  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-#  OTHER DEALINGS IN THE SOFTWARE.
-#
-########################################################################
 
 import sys, os.path, optparse
 
@@ -253,3 +232,5 @@ def main ():
 
 if __name__ == '__main__':
     main()
+
+# vim:set filetype=python shiftwidth=4 softtabstop=4 expandtab:
