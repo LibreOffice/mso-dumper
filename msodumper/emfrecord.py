20ca1d9b734f333d00066724fb5488dfa8b06d40
Miklos Vajna <vmiklos@collabora.co.uk>
2014-04-25 16:04:15 +0200

    dump EmrStrokeandfillpath

--- msodumper/emfrecord.py
+++ msodumper/emfrecord.py
@@ -483,6 +483,20 @@ class EmrFillpath(EMFRecord):
         assert self.pos - posOrig == self.Size
 
 
+class EmrStrokeandfillpath(EMFRecord):
+    """The EMR_STROKEANDFILLPATH record closes any open figures in a path, strokes the outline of the
+    path by using the current pen, and fills its interior by using the current brush."""
+    def __init__(self, parent):
+        EMFRecord.__init__(self, parent)
+
+    def dump(self):
+        posOrig = self.pos
+        self.printAndSet("Type", self.readuInt32())
+        self.printAndSet("Size", self.readuInt32(), hexdump=False)
+        wmfrecord.RectL(self, "Bounds").dump()
+        assert self.pos - posOrig == self.Size
+
+
 class EmrEof(EMFRecord):
     """Indicates the end of the metafile and specifies a palette."""
     def __init__(self, parent):
@@ -669,7 +683,7 @@ RecordType = {
     0x0000003C: ['EMR_ENDPATH', EmrEndpath],
     0x0000003D: ['EMR_CLOSEFIGURE', EmrClosefigure],
     0x0000003E: ['EMR_FILLPATH', EmrFillpath],
-    0x0000003F: ['EMR_STROKEANDFILLPATH'],
+    0x0000003F: ['EMR_STROKEANDFILLPATH', EmrStrokeandfillpath],
     0x00000040: ['EMR_STROKEPATH'],
     0x00000041: ['EMR_FLATTENPATH'],
     0x00000042: ['EMR_WIDENPATH'],
